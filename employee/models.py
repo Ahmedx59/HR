0a2238b8b0241eb937d265fa00ca603ed575b1eb
Ahmedx59 <Ahmedx59@gmail.com>
2024-09-23 00:55:59 +0300

    update employee

--- employee/models.py
+++ employee/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Employee(models.Model):
     name = models.CharField(max_length=50)
-    salary = models.IntegerField(max_length=10)
+    salary = models.IntegerField()
     birth_day = models.DateField
-    age = models.IntegerField(max_length=2)
-    position_id = models.ForeignKey('Position',related_name=position_id , on_delete=models.CASCADE)
+    age = models.IntegerField()
+    position_id = models.ForeignKey('Position',related_name='position_id' , on_delete=models.CASCADE ,blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -13,7 +13,7 @@ class Employee(models.Model):
 
 class Position(models.Model):
     name = models.CharField(max_length=50)
-    years_of_experience = models.IntegerField(max_length=5)
+    years_of_experience = models.IntegerField()
     desc = models.TextField(max_length=500)
 
     def __str__(self):
